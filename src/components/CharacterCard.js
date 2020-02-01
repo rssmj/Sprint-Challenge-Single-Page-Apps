4f83265eb252937ef8e094d60db6e0d49aafcc9c
Michael Ross <mross.j@gmail.com>
2020-02-01 07:28:55 -0500

    display data, add router, display character elements, add styling, add search form ;

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,72 @@
-import React from "react";
+import React from 'react';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+import {
+	Card,
+	CardBody,
+	CardHeader,
+	CardImg,
+	CardText,
+	Col,
+	Row
+} from 'reactstrap';
+
+export default function CharacterCard(props) {
+	return (
+		<Card className='cardsCardsCards'>
+			<CardBody>
+				<CardHeader>
+					<CardHeader>
+						<Card></Card>
+					</CardHeader>
+					<div class=' p-3 d-flex justify-content-center'>
+						<CardHeader>
+							<Col>
+								<Row>
+									<Col>
+										<Row>
+											<Card>
+												<CardBody>
+													<CardHeader>
+														<Card>
+															<CardHeader>
+																<h2>{props.name}</h2>{' '}
+																<Card>
+																	<CardHeader>
+																		<CardText>Status: {props.status}</CardText>
+																	</CardHeader>
+																</Card>
+															</CardHeader>
+														</Card>
+
+														<Card>
+															<CardHeader>
+																<CardBody>
+																	<CardImg src={props.img} />
+																</CardBody>
+															</CardHeader>
+														</Card>
+														<Card>
+															<CardHeader>Species: {props.species}</CardHeader>
+															<CardHeader>Origin: {props.origin}</CardHeader>
+															<CardHeader>
+																Location: {props.location}
+															</CardHeader>
+														</Card>
+													</CardHeader>
+												</CardBody>
+											</Card>
+										</Row>
+									</Col>
+								</Row>
+							</Col>
+						</CardHeader>
+					</div>
+					<CardHeader>
+						<Card></Card>
+					</CardHeader>
+				</CardHeader>
+			</CardBody>
+		</Card>
+	);
 }
+// <-- ^^ y u do dis? ^^ --> //
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,54 @@
-import React, { useState } from "react";
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
+import React, { useState, useEffect } from 'react';
+import { Form, FormControl } from 'react-bootstrap';
 
 export default function SearchForm() {
- 
-  return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
+	const [data, setData] = useState([]);
+	const [query, setQuery] = useState('');
+
+	useEffect(() => {
+		axios
+			.get(`https://rickandmortyapi.com/api/character/`, {
+				params: { key: 'id' }
+			})
+			.then(response => {
+				console.log(response);
+				const searchData = response.data.results.filter(character => {
+					return character.name.toLowerCase().includes(query.toLowerCase());
+				});
+				setData(searchData);
+			});
+	}, [query]);
+	const handleInputChange = event => {
+		setQuery(event.target.value);
+	};
+	return (
+		<section className='search-form'>
+			<div class='lg mb-5 bd-glow'>
+				<Form>
+					<FormControl
+						type='text'
+						name='name'
+						value={query}
+						onChange={handleInputChange}
+						placeholder='Search Rick & Morty!!'
+					/>
+				</Form>
+				<div class='card bg-faded mb-3'></div>
+			</div>
+
+			{data.map(char => (
+				<CharacterCard
+					key={char.id}
+					name={char.name}
+					status={char.status}
+					img={char.image}
+					species={char.species}
+					origin={char.origin.name}
+					location={char.location.name}
+				/>
+			))}
+		</section>
+	);
 }
